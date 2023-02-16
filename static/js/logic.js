b58e1f26a8e55c4159ceaf00e5ecb5d85f760741
BeckyK <barimonsterbk@gmail.com>
2023-02-16 11:04:08 -0600

    adding country polygons

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,32 @@
 
-var base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
 
-var myMap = L.map("map", {
-    center: [0, 12],
-    zoom: 3,
-    layers: [base]
-});
+function createMap(countries){
 
-attrs = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'valence', 'tempo', 'duration'];
-for(i=0; i<attrs.length; i++){
-    attr = attrs[i];
-    d3.select('#attributes').append('button').text(attr).property('value', attr.toString());
+    var base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    var myMap = L.map("map", {
+        center: [0, 12],
+        zoom: 3,
+        layers: [base]
+    });
+
+    attrs = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'valence', 'tempo', 'duration'];
+    for(i=0; i<attrs.length; i++){
+        attr = attrs[i];
+        d3.select('#attributes').append('button').text(attr).property('value', attr.toString());
+    }
+
 }
+
+function createMarkers(polys) {
+    console.log(polys);
+
+
+    
+    createMap(countries);
+}
+
+var polys = 'static/js/countries.geojson'
+d3.json(polys).then(createMarkers);
