5193c24e98b7b2f49927e9e3ec27e744d34483cc
BeckyK <barimonsterbk@gmail.com>
2023-02-15 22:59:38 -0600

    create basic map & buttons

---
+++ static/js/logic.js
@@ -0,0 +1,16 @@
+
+var base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var myMap = L.map("map", {
+    center: [0, 12],
+    zoom: 3,
+    layers: [base]
+});
+
+attrs = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'valence', 'tempo', 'duration'];
+for(i=0; i<attrs.length; i++){
+    attr = attrs[i];
+    d3.select('#attributes').append('button').text(attr).property('value', attr.toString());
+}
