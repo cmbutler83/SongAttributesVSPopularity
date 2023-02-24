ed625e48c043b00ed28af0f4cbb84dd0ddc60e92
BeckyK <barimonsterbk@gmail.com>
2023-02-23 18:30:32 -0600

    add attribute info to legend

---
+++ static/js/attribute_info.js
@@ -0,0 +1,8 @@
+var attDeets = {'DANCEABILITY': 'Range of 0.0-1.0 of how suitable a song is for dancing, based on a combo of rhythm stability, beat strength, and overall regularity', 
+    'ENERGY': 'Range of 0.0-1.0 representing intensity and activity of song',
+    'LOUDNESS': 'Average overall loudness of song in dBs compared to relative loudness of all tracks', 
+    'SPEECHINESS': 'Presence of spoken word in a song, ranges above .66 are completely speech', 
+    'ACOUSTICNESS': 'Confidence measure from 0.0-1.0 of whether the song is acoustic', 
+    'VALENCE': 'Range from 0.0-1.0 of music positiveness, high is cheerful and happy, low is angry or sad', 
+    'TEMPO': 'Overall average estimated beats per minute (BPM) for full song', 
+    'DURATION': 'Length of track in seconds'};
