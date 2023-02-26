9100b3e520a74abe6dca45dcc298cf119325f8f6
BeckyK <barimonsterbk@gmail.com>
2023-02-25 21:53:42 -0600

    FINALLY got table colors working properly!

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,11 @@
 // Global variables
-var attrs;
 var countryData;
 var myMap;
-var first = true;
 var legend;
-var c;
 var infoBox;
+var attrs;
+var c;
+var first = true;
 
 // Define function to create map
 function createMap(countries){
@@ -13,7 +13,7 @@ function createMap(countries){
     // Map tiles
     var base = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>)'
-    })
+    });
 
     // Create map
     myMap = L.map("map", {
@@ -39,12 +39,13 @@ function createMap(countries){
 // Define function to create interactive buttons for song attributes
 function createButtons(){
 
+    // Add button to switch between map page & main dashboard
     d3.select('#top').append('a').property('href', '/')
     .property('id', 'home')
     .text('Back to Dashboard Page');
 
+    // Add title
     d3.select('#top').append('h1').property('id', 'pageTitle').text('Song Attribute Popularity Distribution (Top 50 Songs per Country)')
-
 
     // Array to hold song attributes
     attrs = ['DANCEABILITY', 'ENERGY', 'LOUDNESS', 'SPEECHINESS', 'ACOUSTICNESS', 'VALENCE', 'TEMPO', 'DURATION'];
@@ -58,15 +59,15 @@ function createButtons(){
             .property('value', attr.toString())
             .property('id', `button${i}`)
             .attr('onclick', 'buttonPressed(this.value)');
-    }
-}
+    };
+};
 
 // Define function to update map on button clicks
 function updateMap(countries, att){
     if(!first){
         // remove previous legend if exists
         legend.remove(myMap);
-    }
+    };
     // Define new legend
     legend = L.control({position: 'topright'});
     legend.onAdd = function(){
@@ -101,10 +102,13 @@ function updateMap(countries, att){
     first = false;
 };
 
+// Define function to convert country data to table format
 function createTable(data){
 
+    // store column names
     var columns = ['attribute', 'value', 'average'];
 
+    // create table and sections
     var table = d3.select('#attrs').append('table');
 	var thead = table.append('thead');
 	var	tbody = table.append('tbody');
@@ -122,21 +126,59 @@ function createTable(data){
 	var cells = rows.selectAll('td').data(function (row) {
 	    return columns.map(function (column) {
 	      return {column: column, value: row[column]};
-	    });
-	  }).enter().append('td').text(d => d.value);
+	    })
+	  }).enter().append('td').text(d => d.value)
     
+    // rename columns
     thead.selectAll('th').text(function(column){
         if(column === 'attribute'){
             return ' '
         } else if (column === 'value'){
-            return 'This Country | '
+            return 'This Country'
         } else {
-            return ' All Countries'
+            return 'All Countries'
         }
-    })
+    });
 
-  return table;
-}
+    // color table according to values here...
+    //cells.style('color', 'red');
+
+    var thisOne = [];
+    var trs = d3.selectAll('tr').data();
+    var tds = d3.selectAll('tr');
+    for(t = 1; t < trs.length; t++){
+        var val1 = parseFloat(trs[t].value);
+        var val2 = parseFloat(trs[t].average);
+        console.log(val1, val2);
+        if(val2 > val1){
+            thisOne.push(false);
+        } else if(val1 > val2){
+            thisOne.push(true);
+        } else {
+            thisOne.push(null);
+        }
+    }
+    console.log(thisOne);
+    for(i = 0; i < thisOne.length; i++){
+        var row = document.getElementsByTagName('tr')[i+1];
+        if(thisOne[i] === null){
+            
+        } else if(thisOne[i]){
+            row.cells[1].style.color = 'dodgerblue'
+            row.cells[2].style.color = 'darkorange'
+        } else if(!thisOne[i]){
+            row.cells[1].style.color = 'darkorange'
+            row.cells[2].style.color = 'dodgerblue'
+        }
+    }
+    
+    // const tds = d3.selectAll('td').each(function() {
+    //     d3.select(this).style('color',
+    //     this.innerHTML > 0 ? 'dodgerblue' : 'darkorange')
+    // })
+
+    
+};
 
 
 // Define function to draw country borders
@@ -199,38 +241,23 @@ function createMarkers(polys, att) {
             fetch(url).then(response => response.json())
             .then((json) => {
 
-                console.log(json)
                 let stats = json
                 if(stats[0] !== undefined){
 
                     if(clickd === 'China'){
-                        document.getElementById('attrs').innerHTML = '(Spotify blocked in mainland China, data is for Hong Kong ONLY)'
+                        document.getElementById('attrs').innerHTML = '(Spotify blocked in mainland China, data is for Hong Kong ONLY)<br><br><hr><br>'
                         document.getElementById('this_country').innerHTML = 'Hong Kong'
                     } else {
                         document.getElementById('attrs').innerHTML = '';
-                        document.getElementById('this_country').innerHTML = clickd
+                        document.getElementById('this_country').innerHTML = clickd + '<hr>'
                     }
-                    createTable(json);
-                    
-                    // d3.select('#attrs').append('p').text(`Danceability: ${stats.danceability.value}, ${parseFloat(stats.danceability.average).toFixed(2)}`)
-                    // d3.select('#attrs').append('p').text(`Energy: ${stats.energy.value}, ${parseFloat(stats.energy.average).toFixed(2)}`)
-                    // d3.select('#attrs').append('p').text(`Loudness: ${stats.loudness.value}, ${parseFloat(stats.loudness.average).toFixed(2)}`)
-                    // d3.select('#attrs').append('p').text(`Speechiness: ${stats.speechiness.value}, ${parseFloat(stats.speechiness.average).toFixed(2)}`)
-                    // d3.select('#attrs').append('p').text(`Acousticness: ${stats.acousticness.value}, ${parseFloat(stats.acousticness.average).toFixed(2)}`)
-                    // d3.select('#attrs').append('p').text(`Valence: ${stats.valence.value}, ${parseFloat(stats.valence.average).toFixed(2)}`)
-                    // d3.select('#attrs').append('p').text(`Tempo: ${stats.tempo.value}, ${parseFloat(stats.tempo.average).toFixed(2)}`)
-                    // d3.select('#attrs').append('p').text(`Duration: ${stats.duration.value}, ${parseFloat(stats.duration.average).toFixed(2)}`)
+                    var sortd = json.sort((a, b) => Math.abs(b.average - b.value) - Math.abs(a.average - a.value));
+                    createTable(sortd);
                 }
                 else {
                     document.getElementById('attrs').innerHTML = `No Spotify data for ${clickd}, please try another country.`
                     document.getElementById('this_country').innerHTML = clickd  
                 }
-            
-
-            
-
-            
-            
             });
         });
     };
@@ -338,7 +365,7 @@ function buttonPressed(attribute){
     let url = `/attributes/${attribute}`
     fetch(url).then(response => response.json())
     .then(json => {
-        console.log(json);
+
         // Join JSON to existing GeoJSON data
         let joinedGJ = JSON.parse(JSON.stringify(countryData));
         for (let i = 0; i < joinedGJ.features.length; i++){
